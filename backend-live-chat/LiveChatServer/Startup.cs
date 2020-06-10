6b8450e5cd533eb7280370f49c674623507d37c0
Alef Silva <alef.silva@medroom.com.br>
2020-06-10 17:55:10 -0300

    Adding SignalR to Startup services

--- backend-live-chat/LiveChatServer/Startup.cs
+++ backend-live-chat/LiveChatServer/Startup.cs
@@ -16,6 +16,7 @@ namespace LiveChatServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
