ba046661adea7d624d6e44adb18e1e272fdb07a8
Alef Silva <alef.silva@medroom.com.br>
2020-06-10 18:38:16 -0300

    Mapping LiveChatHub route at Startup

--- backend-live-chat/LiveChatServer/Startup.cs
+++ backend-live-chat/LiveChatServer/Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using LiveChatServer.Hubs;
 
 namespace LiveChatServer
 {
@@ -31,10 +27,7 @@ namespace LiveChatServer
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<LiveChatHub>("/liveChatHub");
             });
         }
     }
