4b4d9e6ef09903f575a464f6c52792a5bee30594
Alef Silva <alef.silva@medroom.com.br>
2020-06-10 20:55:29 -0300

    live-chat-component

---
+++ front-end-live-chat/live-chat-signalr/src/app/views/live-chat/live-chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'live-chat',
+    templateUrl: 'live-chat.component.html',
+    styleUrls: ['live-chat.component.scss']
+})
+export class LiveChatComponent {
+
+
+}
