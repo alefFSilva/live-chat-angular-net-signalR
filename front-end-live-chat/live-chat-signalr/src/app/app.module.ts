481802958eeb22c2407d7529a8e8e11573d73fac
Alef Silva <alef.silva@medroom.com.br>
2020-06-10 19:24:07 -0300

    Adding home component

--- front-end-live-chat/live-chat-signalr/src/app/app.module.ts
+++ front-end-live-chat/live-chat-signalr/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
---
+++ front-end-live-chat/live-chat-signalr/src/app/views/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home-component',
+    templateUrl: 'home.component.html',
+    styleUrls: [ 'home.component.scss']
+})
+export class HomeComponent {
+
+
+}
