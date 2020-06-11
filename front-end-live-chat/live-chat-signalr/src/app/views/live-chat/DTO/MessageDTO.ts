6c058f6d6cead90eb245b55efa69ea684f7fb69a
Alef Silva <alef.silva@medroom.com.br>
2020-06-10 23:08:53 -0300

    MessageDTO  - A little refactoring

--- front-end-live-chat/live-chat-signalr/src/app/views/live-chat/DTO/MessageDTO.ts
+++ front-end-live-chat/live-chat-signalr/src/app/views/live-chat/DTO/MessageDTO.ts
@@ -3,5 +3,11 @@ import { MessageTypeEnum } from '../Enums/MessageTypeEnum';
 export class MessageDTO {
     public userName: string;
     public content: string;
-    public messageType: MessageTypeEnum;
+    public type: MessageTypeEnum;
+
+    constructor(userName: string, content: string, type: MessageTypeEnum) {
+        this.userName = userName;
+        this.content = content;
+        this.type = type;
+    }
 }
