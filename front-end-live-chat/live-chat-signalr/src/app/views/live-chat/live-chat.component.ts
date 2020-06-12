ba7ae6624e59a0e0724b2a3511644d0052a5439d
Alef Silva <alef.silva@medroom.com.br>
2020-06-11 21:57:36 -0300

    Applying scrollDown msgs and message input clean

--- front-end-live-chat/live-chat-signalr/src/app/views/live-chat/live-chat.component.ts
+++ front-end-live-chat/live-chat-signalr/src/app/views/live-chat/live-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { LiveChatService } from './../../services/live-chat-service';
 import { MessageDTO } from './DTO/MessageDTO';
 import { MessageTypeEnum } from './Enums/MessageTypeEnum';
@@ -8,17 +8,24 @@ import { MessageTypeEnum } from './Enums/MessageTypeEnum';
     templateUrl: 'live-chat.component.html',
     styleUrls: ['live-chat.component.scss']
 })
-export class LiveChatComponent implements OnInit {
-
+export class LiveChatComponent implements OnInit, AfterViewChecked {
     public messageTypeEnumRef: typeof MessageTypeEnum;
     public chatMessages: MessageDTO[];
 
+    @ViewChild('messagesContainer')
+    private _messagesContainer: ElementRef;
     private _liveChatService: LiveChatService;
 
     constructor(liveChatService: LiveChatService){
         this.chatMessages = [];
         this.messageTypeEnumRef = MessageTypeEnum;
         this._liveChatService = liveChatService;
+    }
+
+    public ngAfterViewChecked(): void {
+        if (this._messagesContainer && this.chatMessages.length > 0){
+            this.scrollPageToBottom()
+        }
     }
 
     public ngOnInit(): void {
@@ -30,10 +37,17 @@ export class LiveChatComponent implements OnInit {
         });
     }
 
-    public sendNewMessage(messageContent: string): void {
+    public sendNewMessage(messageInput: HTMLInputElement): void {
+        const messageContent = messageInput.value;
         const currentUserName = this._liveChatService.CurrentUserName;
         const newMessage = new MessageDTO(currentUserName, messageContent, MessageTypeEnum.CurrentUserMessage);
         this.chatMessages.push(newMessage);
         this._liveChatService.sendNewMessage(messageContent);
+        messageInput.value = '';
+    }
+
+    private scrollPageToBottom(): void {
+        this._messagesContainer.nativeElement.scrollTop =
+        this._messagesContainer.nativeElement.scrollHeight;
     }
 }
