26a43b8354d9304addb603042bb77706bc6a463c
Alef Silva <alef.silva@medroom.com.br>
2020-06-11 23:28:33 -0300

    Applying icon to send message button

--- front-end-live-chat/live-chat-signalr/src/app/app.module.ts
+++ front-end-live-chat/live-chat-signalr/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LiveChatComponent } from './views/live-chat/live-chat.component';
 import { LiveChatService } from './services/live-chat-service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
 import { HomeComponent } from './views/home/home.component';
 import { APP_ROUTES } from './routes/APP_ROUTES';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,8 @@ import { APP_ROUTES } from './routes/APP_ROUTES';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES),
+    FontAwesomeModule
   ],
   providers: [ LiveChatService ],
   bootstrap: [ AppComponent ]
--- front-end-live-chat/live-chat-signalr/src/app/views/live-chat/live-chat.component.ts
+++ front-end-live-chat/live-chat-signalr/src/app/views/live-chat/live-chat.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@ang
 import { LiveChatService } from './../../services/live-chat-service';
 import { MessageDTO } from './DTO/MessageDTO';
 import { MessageTypeEnum } from './Enums/MessageTypeEnum';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'live-chat',
@@ -11,6 +13,7 @@ import { MessageTypeEnum } from './Enums/MessageTypeEnum';
 export class LiveChatComponent implements OnInit, AfterViewChecked {
     public messageTypeEnumRef: typeof MessageTypeEnum;
     public chatMessages: MessageDTO[];
+    public sendMessageIcon: IconDefinition;
 
     @ViewChild('messagesContainer')
     private _messagesContainer: ElementRef;
@@ -20,6 +23,7 @@ export class LiveChatComponent implements OnInit, AfterViewChecked {
         this.chatMessages = [];
         this.messageTypeEnumRef = MessageTypeEnum;
         this._liveChatService = liveChatService;
+        this.sendMessageIcon = faArrowAltCircleRight;
     }
 
     public ngAfterViewChecked(): void {
